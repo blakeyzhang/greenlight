d33c391df6f7b8e55bcb20d1becaab5cde36514b
zhangyiming <zhangyiming@zhangyimingdeMacBook-Pro.local>
2024-05-24 18:40:24 +0800

    add models

---
+++ README.md
@@ -0,0 +1,10 @@
+# greenlight
+
+
+
+## Database Config
+
+1. dsn value config:
+```bash
+export GREENLIGHT_DB_DSN=postgres://greenlight:19910203@localhost/greenlight?sslmode=disable
+```
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,11 @@
 package data
 
-import "time"
+import (
+	"database/sql"
+	"time"
+
+	"greenlight.lineway.site/internal/validator"
+)
 
 type Movie struct {
 	ID       int64     `json:"id"`
@@ -10,4 +15,39 @@ type Movie struct {
 	Runtime  Runtime   `json:"runtime,omitempty"`
 	Genres   []string  `json:"genres.omitempty"`
 	Version  int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+
+}
+
+type MovieModel struct {
+	DB *sql.DB
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
 }
